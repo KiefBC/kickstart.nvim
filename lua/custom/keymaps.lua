d2570e20a8ffa6e05afd0dbf6e325dcf203e2762
Kiefer <kiefx@pm.me>
2024-10-25 12:26:27 -0700

    clangd-15 now working as my cpp LSP

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,1 +1,4 @@
+-- This will show us the Directory Strcuture
 vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', { desc = 'Fil[E] Tree' })
+-- This will allow us to Shift-K and get the Hover property to show
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
